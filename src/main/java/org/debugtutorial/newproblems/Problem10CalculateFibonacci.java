f051802129d81371cebbcee396b3cc2f60d1cf8e
sharmisthachoudhuri <sharmistha.choudhuri193@gmail.com>
2022-06-03 00:01:19 +0530

    Added new problem samples

---
+++ src/main/java/org/debugtutorial/newproblems/Problem10CalculateFibonacci.java
@@ -0,0 +1,21 @@
+package org.debugtutorial.newproblems;
+
+/**
+ * The following method intends to compute the n-th Fibonacci number,
+ * but contains a bug for the first two numbers
+ */
+public class Problem10CalculateFibonacci {
+    static int fib(int n) {
+        int f = 0;
+        int f0 = 1;
+        int f1 = 1;
+
+        while (n > 1) {
+            n--;
+            f = f0 +f1;
+            f0 = f1;
+            f1 = f;
+        }
+        return f;
+    }
+}
