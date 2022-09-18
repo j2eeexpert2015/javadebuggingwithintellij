675f3741efbea110bb3346e9ff1db6873bd0f18e
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-09-18 12:51:36 +0530

    Added changes

--- src/main/java/org/debugtutorial/advancedproblemsolving/solutions/LowerTriangularMatrix.java
+++ src/main/java/org/debugtutorial/advancedproblemsolving/solutions/LowerTriangularMatrix.java
@@ -22,11 +22,23 @@ public class LowerTriangularMatrix {
 			System.out.println();
 		}
 	}
+	
+	static void printMatrix(int matrix[][]) {
+		for (int i = 0; i < matrix.length; i++) { //this equals to the row in our matrix.
+	         for (int j = 0; j < matrix[i].length; j++) { //this equals to the column in each row.
+	            System.out.print(matrix[i][j] + " ");
+	         }
+	         System.out.println(); //change line on console as row comes to end in the matrix.
+	      }
+	}
 
 	// Driver Code
 	public static void main(String args[]) {
 		int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 		int row = 3, col = 3;
+		
+		System.out.println("Initial matrix: ");
+		printMatrix(matrix);
 
 		System.out.println("Lower triangular matrix: ");
 		lowerTriangularMatrix(matrix, row, col);
