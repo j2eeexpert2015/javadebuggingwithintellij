ef6b6d20426c9ba16214c4ab14ef623263b224ad
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-09-18 12:48:18 +0530

    Added Changes

--- src/main/java/org/debugtutorial/advancedproblemsolving/problems/LeftStarTriangle.java
+++ src/main/java/org/debugtutorial/advancedproblemsolving/problems/LeftStarTriangle.java
@@ -14,7 +14,7 @@ public class LeftStarTriangle {
 		int row = 6;
 		for (int i = 1; i <= row; i++) {
 			for (int j = 1; j <= row - i; j++) {
-				System.out.print(" ");
+				System.out.print("*");
 			}
 			for (int k = 1; k <= i; k++) {
 				System.out.print("*");
