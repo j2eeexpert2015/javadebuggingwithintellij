47d41b0ac1257a70311d2837d93ca6f1faef25c6
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-12-25 11:02:34 +0530

    Restructured problem solving

---
+++ src/main/java/org/debugtutorial/problemsolving/advanced/TicTacToeProblem.java
@@ -0,0 +1,50 @@
+package org.debugtutorial.problemsolving.advanced;
+
+/**
+ * Your code was supposed to produce the following output:
+ *
+ * X _ O
+ *
+ * _ O _
+ *
+ * O _ X
+ *
+ * O WINS!
+ *
+ * Instead, your code produces:
+ *
+ * X _ O
+ *
+ * _ X _
+ *
+ * O _ X
+ *
+ * O WINS!
+ */
+public class TicTacToeProblem {
+    public static void main(String[] args) {
+        char[][] array = {
+            {'_', '_', '_'},
+            {'_', '_', '_'},
+            {'_', '_', '_'}
+        };
+        
+        for (int i = 0; i < array.length; i++) {
+               array[i][2-i] = 'O';
+               array[2-i][2-i] = 'X';
+        }
+
+        printArray(array);
+        System.out.println("\nO WINS!\n");
+   }
+   
+   public static void printArray(char[][] array) {
+       System.out.println();
+       for (int i = 0; i < array.length; i++) {
+           for (int j = 0; j < array[i].length; j++) {
+               System.out.print( array[i][j] + "  ");
+           }
+           System.out.println();
+       }
+   }
+}
